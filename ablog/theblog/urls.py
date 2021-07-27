901a6c3e557a12fdd8fca64f05ec557f058605dd
Edmond22-prog <ghislain.makolle@facsciences-uy1.cm>
2021-07-27 02:13:52 +0100

    codemy video #2

--- ablog/theblog/urls.py
+++ ablog/theblog/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
-from . import views
+#from . import views
+from .views import HomeView, ArticleDetailView
 
 urlpatterns = [
-    path('', views.home, name="home"),
+    #path('', views.home, name="home"),
+    path('', HomeView.as_view(), name="home"),
+    # Lorsque l'on cree un nouveau Poste, il a une cle primaire lorsqu'il est enregistre dans
+    # dans la base de donnees, c'est cette cle primaire qu'on recupere ici avec "pk"
+    path('article/<int:pk>', ArticleDetailView.as_view(), name="article-detail"),
 ]
--- ablog/theblog/views.py
+++ ablog/theblog/views.py
@@ -1,5 +1,19 @@
+from .models import Post
 from django.shortcuts import render
+# Importation des vues générique de Django : ListView et DetailView
+from django.views.generic import ListView, DetailView
 
 # Create your views here.
-def home (request):
-    return render(request, 'home.html', {})
+""" def home (request):
+    return render(request, 'home.html', {}) """
+
+# Définition d'une classe HomeView qui permettra d'organiser les différents composants d'une page
+# en ListView, c'est à dire en liste descendante
+class HomeView(ListView):
+    model = Post
+    template_name = 'home.html'
+
+
+class ArticleDetailView(DetailView):
+    model = Post
+    template_name = 'article_details.html'
