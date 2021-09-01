0936827469c0a9466590a07a467eb7595f7b7bed
Edmond22-prog <ghislain.makolle@facsciences-uy1.cm>
2021-09-01 01:31:20 +0100

    codemy video #23

--- ablog/members/urls.py
+++ ablog/members/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import UserRegisterView
+from .views import UserEditView, UserRegisterView
 
 
 urlpatterns = [
     path('register/', UserRegisterView.as_view(), name="register"),
+    path('edit_profile/', UserEditView.as_view(), name="edit-profile"),
 ]
--- ablog/members/views.py
+++ ablog/members/views.py
@@ -2,7 +2,7 @@ from .forms import SignUpForm
 from typing import Generic
 from django.shortcuts import render
 from django.views import generic
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 from django.urls import reverse_lazy
 
 
@@ -10,3 +10,13 @@ class UserRegisterView(generic.CreateView):
     form_class = SignUpForm
     template_name = 'registration/register.html'
     success_url = reverse_lazy('login')
+
+class UserEditView(generic.UpdateView):
+    form_class = UserChangeForm
+    template_name = 'registration/edit_profile.html'
+    success_url = reverse_lazy('home')
+
+    # Cette fonction remplit toutes les entrees du formulaire avec les informations de 
+    # l'utilisateur actuel a modifier
+    def get_object(self):
+        return self.request.user
